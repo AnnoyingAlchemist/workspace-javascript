e9a2b0d5608fcef400360995e37409d3a7439b97
brandon <bpayero@usca.edu>
2022-10-24 13:17:12 -0400

    Oct 24 Practice

---
+++ js/quiz.js
@@ -0,0 +1,14 @@
+//event handlers
+function changeStyle(){
+    var classname = this.getAttribute("class");
+    if (classname =="answer") {
+        this.setAttribute("class", "answer selected"); 
+    } else {
+        this.setAttribute("class", "answer"); 
+    }
+    
+}
+//document.querySelectorAll('.answer');
+for (var i = 0; i < 4; i++) {
+    document.getElementsByClassName('answer')[i].addEventListener("change", changeStyle, false);    
+}
