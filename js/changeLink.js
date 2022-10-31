dab167dad82b8d80d69700781c7ecc58cb1c3f4b
Brandon <bpayero@usca.edu>
2022-10-31 13:19:37 -0400

    Halloween Practice

---
+++ js/changeLink.js
@@ -0,0 +1,13 @@
+/* try the jQuery */
+$(function(){   //document.ready
+
+$("a").each(function(){
+    $(this).attr('href',"https://www.google.com");
+    $(this).css('text-decoration','line-through');
+}
+);
+//Fix image path
+
+
+
+});
