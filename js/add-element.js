e5ebdbded000df992f3e40f5950952383f2aa85b
Brandon <bpayero@usca.edu>
2022-11-07 13:12:48 -0500

    Nov 7 Practice

---
+++ js/add-element.js
@@ -0,0 +1,6 @@
+//testing the class
+$('.hot').each(function(){
+    $(this).removeClass("hot");
+    $(this).addClass("cool");
+    
+});
--- js/hotel.js
+++ js/hotel.js
@@ -12,13 +12,40 @@ console.log("Name: "+test.hotelname);
 console.log("Rooms: "+test.room);
 //event
 $(function() {
-    $('submit').click(reservation);
+    $('#submit').click(reservation);
+    $('#lists').click(displayR);
 });
 
+var guestgroup = new Array();
 function reservation() {
     var guest = {
-        name: $('name').val(),
-        numberofGuest: $('#num').val(),
-        roomType: $('room').val(),
+        name: $('#name').val(),
+        numberOfGuest: $('#numGuest').val(),
+        roomType: $('#room').val(),
+        checkIn: $('#checkin').val(),
+        checkOut: $('#checkout').val()
     };
+    $("p:first").html("thank you!");
+
+    guestgroup.push(guest); //store in the array
+    //console logs
+    console.log(guestgroup + "\n")
+    console.log("name: "+ guest.name);
+    console.log("Guest number: "+ guest.numberOfGuest);
+    console.log("Room Type: "+ guest.roomType);
+    console.log("Check in date: "+ guest.checkIn);
+    console.log("Check out date: "+ guest.checkOut + "\n");
+}
+
+function displayR() {
+    $('form').append("<table>");
+    for (var i in guestgroup) {
+        var theguest = guestgroup[i];
+        //console.log call
+        console.log("theguest"+"#"+i+": "+theguest)
+
+        var row = "<tr><td>"+theguest.name+"</td><td>"+theguest.checkIn+"</td></tr>";
+        $('table').append(row);
+        }
+    
 }
